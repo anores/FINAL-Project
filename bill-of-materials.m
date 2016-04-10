3c61d4c0e0a27a5a7de621da4d7a8867677324d1
anores <ahurtado@cca.edu>
2016-04-10 00:15:37 -0700

    Create bill-of-materials.m

---
+++ bill-of-materials.m
@@ -0,0 +1,9 @@
+1. 1m LED color strip (http://goo.gl/5NE7wf) (arrives Tuesday Apr,12)
+2. Plastic for 3D-printing plastic for planters - getting from Hybrid Lab
+3. 3D printing blueprints for planters - Thingverse.com
+4. 3D printing software - CAD.com, Makerbot.com...
+5. 3 small pot plants with soil - Home depot, Lowes, plant nursery...
+6. "Soil Moisture" Component - getting from Hybrid Lab
+7. "Motion" detector component - getting from Hybrid Lab
+8. Arduino board - Already have it
+9. Other miscellaneous componets (wires, resistors, etc) - getting from Hybrid Lab
